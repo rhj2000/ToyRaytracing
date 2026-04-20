e001f46cc43c295b62a404e381940261cd66f9d3
Riley H <78668952+rhj2000@users.noreply.github.com>
2026-04-20 09:41:36 -0400

    Sphere w/ surface normals.

--- main.cpp
+++ main.cpp
@@ -10,8 +10,44 @@
 #include<chrono>
 #endif DEBUG
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+	
+	vec3 point2center{ center - r.origin() }; 
+	
+	auto a{dot(r.direction(), r.direction())};
+	auto b{ -2 * dot(r.direction(), point2center) };
+	auto c{dot(point2center, point2center) - radius * radius};
+
+	auto discriminant{b * b - 4 * a * c}; 
+
+	if (discriminant < 0) 
+	{
+		return -1.0;
+	}
+	else
+	{
+		return (-b - std::sqrt(discriminant)) / (2.0 * a);
+	}
+
+}
+
 color ray_color(const ray& r) {
-	return color(0, 0, 0);
+	auto sphere_center{ point3(0,0,-1) };
+	auto t{ hit_sphere(sphere_center, 0.5, r) };
+	if (t > 0.0) {
+		vec3 surface_normal{ unit_vector(r.at(t) - sphere_center) };
+		return 0.5 * color(surface_normal.x() + 1, 
+						   surface_normal .y() + 1, 
+						   surface_normal.z() + 1); //domain [0,1]
+	}
+	
+
+	vec3 unit_direction{ unit_vector(r.direction()) }; //y = [-1,1]
+
+	// do linear interpolation between white and blue
+	auto a{ unit_direction.y() + 1.0 }; //y = [0,2]
+	a *= 0.5; //y = [0,1]
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -45,7 +81,7 @@ int main() {
 	auto viewport_upper_left{ camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2 };
 
 	// the pixel grid starts inset from the viewport edges by half the pixel-to-pixel distance
-	auto pixel00_loc{viewport_upper_left + 0.5 * (viewport_u + viewport_v)};
+	auto pixel00_loc{viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)};
 
 	// Render
 
@@ -58,10 +94,11 @@ int main() {
 		std::clog << "\rScanlines remaining: " << (image_height - i) << " " << std::flush;
 		for (int j{ 0 }; j < image_width; ++j) {
 			auto pixel_center{ pixel00_loc + (j * pixel_delta_u) + (i * pixel_delta_v) };
-			auto ray_direction{ pixel_center - camera_center }; //NOT NORMALIZED!!!
+			auto ray_direction{ pixel_center - camera_center };
+			
 			ray r(camera_center, ray_direction);
-
-			color pixel_color{ ray_color(r) };
+			
+			color pixel_color{ray_color(r)};
 			write_color(std::cout, pixel_color); //MUST be called to write to file
 		}
 	}
